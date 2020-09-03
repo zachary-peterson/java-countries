ea4c536346cbfdb922253c7c20413180efbfb910
Zachary <zp.lambda@yahoo.com>
2020-09-03 18:43:24 -0400

    almost there

--- src/main/java/com/countries/com/countries/controllers/CountryController.java
+++ src/main/java/com/countries/com/countries/controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,6 +19,19 @@ public class CountryController
     private CountryRepository countrepos;
 
     // http://localhost:2019/names/all
+
+    private ArrayList<Country> findCountries(ArrayList<Country> myList, CheckCountry tester)
+    {
+        ArrayList<Country> tempList = new ArrayList<>();
+        for (Country c : myList)
+        {
+            if (tester.test(c))
+            {
+                tempList.add(c);
+            }
+        }
+        return tempList;
+    }
 
     @GetMapping(value = "/names/all", produces = {"application/json"})
     public ResponseEntity<?> listAllCountries()
@@ -32,14 +46,70 @@ public class CountryController
         {
             System.out.println(c);
         }
-        return new ResponseEntity<>(myList, HttpStatus.OK);
+        return new ResponseEntity<>( myList, HttpStatus.OK);
     }
 
     // http://localhost:2019/names/start/u
+
+    @GetMapping(value = "/names/start/{letter}",
+        produces = "application/json")
+    public ResponseEntity<?> listAllByFirstName(@PathVariable char letter)
+    {
+        ArrayList<Country> myList = new ArrayList<>();
+        countrepos.findAll().iterator().forEachRemaining(myList::add);
+        List<Country> rtnList = findCountries(myList, c -> c.getName().toLowerCase().charAt(0) == Character.toLowerCase(letter));
+
+        for (Country c : rtnList)
+        {
+            System.out.println(c);
+        }
+
+        return new ResponseEntity<>(rtnList, HttpStatus.OK);
+    }
+
     // http://localhost:2019/population/total
+
+    @GetMapping(value = "/population/total", produces = {"application/json"})
+    public ResponseEntity<?> displayPopulation()
+    {
+        List<Country> myList = new ArrayList<>();
+
+        countrepos.findAll().iterator().forEachRemaining(myList::add);
+
+        double total = 0.0;
+
+        for (Country c : myList)
+        {
+            total = total + c.getPopulation();
+        }
+        System.out.println("The Total Population Is " + total);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     // http://localhost:2019/population/min
+
+    @GetMapping(value = "/population/min", produces = {"application/json"})
+    public ResponseEntity<?> displayPopulationMin()
+    {
+        ArrayList<Country> myList = new ArrayList<>();
+
+        countrepos.findAll().iterator().forEachRemaining(myList::add);
+        
+        myList.sort((c1, c2) -> (int) (c1.getPopulation() - c2.getPopulation()));
+
+
+        for (Country c : myList)
+        {
+            total = total + c.getPopulation();
+        }
+        System.out.println("The Total Population Is " + total);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
     // http://localhost:2019/population/max
 
     //Stretch
     // http://localhost:2019/population/median
+    //
+
 }
